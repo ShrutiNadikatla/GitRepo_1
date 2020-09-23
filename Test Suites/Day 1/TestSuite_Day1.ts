fc96b282fa80883a2dc14a6bd548b1d934eb85a2
snadikatla <snadikatla@UWORLD-IN-T16.azure.uworld.com>
2020-09-23 19:28:49 +0530

    1st project commit

---
+++ Test Suites/Day 1/TestSuite_Day1.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_Day1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>61899e57-f9b4-49c7-a474-677cb9a85dbb</testSuiteGuid>
+   <testCaseLink>
+      <guid>fa78198c-528d-46a8-939d-b2e0818c6c2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day 1/TestCase_001_Record</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73de1d22-af83-4a48-ba1c-004e5a45b299</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day 1/TestCase_003_Script</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34592358-cc36-487c-a84f-9a5527bc219c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day 1/TestCase_002_Manual</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
